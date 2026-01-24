4e5669bc7775db13ea505dec946985cf47fff5b1
SaleamlakH <saleamlak.7h@gmail.com>
2026-01-24 12:28:58 +0300

    Feat: Add GameBoard factory function with immediate invocation

---
+++ script.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const gameBoard = (function GameBoard() {
+    let board = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    ];
+    
+    // return the gameBoard object 
+    // so other methods can be chained
+    const addMark = function(mark, index) {
+        const [row, column] = index.split('');
+        
+        board[row][column] = mark;
+        return this;
+    }
+
+    const reset = function() {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                board[i][j] = '';
+            }
+        };
+
+        return this;
+    }
+
+    const print = () => {
+        console.table(board);
+    }
+
+    return {board, print, addMark, reset};
+})();
